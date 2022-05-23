7b58326c71008d3ef6a9d82c7e8fe4391d82427b
GunSuebtrangan <gunsuebtrangan@gmail.com>
2022-05-23 17:40:02 +0700

    Update Rocketdetail.tsx

--- component/Rocketdetail.tsx
+++ component/Rocketdetail.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
-import { Space, Table, Tag, Spin } from "antd";
+import { Table, Tag, Spin } from "antd";
 import { ColumnsType } from "antd/lib/table";
 interface objIdRocket {
   idRocket?: string;
@@ -7,29 +7,32 @@ interface objIdRocket {
 
 const Rocketdetail: any = (props: objIdRocket) => {
   // console.log(props.idRocket);
-  let column: any = [
-    { title: "Name", dataIndex: "name", key: "name" },
-    { title: "Description", dataIndex: "description", key: "description" },
-    { title: "Height", dataIndex: "height", key: "height" },
-    ,
-    { title: "Weight", dataIndex: "weight", key: "weight" },
-    { title: "Company", dataIndex: "company", key: "company" },
-  ];
+
   let dataRocketDetail: any = [];
+  let column: any = [];
+  let subColumn: any = [];
   const getRocketDetail = gql`
     query GetRocket {
       rocket(id: "${props.idRocket}") {
         id
         name
         description
+        active
         height {
           feet
           meters
+        }
+        engines {
+          engine_loss_max
+          number
+          layout
+        
         }
         company      
         mass {
           kg
         }
+        wikipedia
       }
     }
   `;
@@ -37,6 +40,51 @@ const Rocketdetail: any = (props: objIdRocket) => {
   if (loading) return <Spin />;
   if (error) return `Error! ${error.message}`;
   if (data.rocket) {
+    column = [
+      { title: "Name", dataIndex: "name", key: "name" },
+      { title: "Description", dataIndex: "description", key: "description" },
+      { title: "Height", dataIndex: "height", key: "height" },
+      ,
+      { title: "Weight", dataIndex: "weight", key: "weight" },
+      { title: "Company", dataIndex: "company", key: "company" },
+      {
+        title: "Active",
+        dataIndex: "active",
+        key: "active",
+        render: (_: any, { tags }: any) => (
+          <>
+            {tags ? (
+              <Tag color={"green"}>Yes</Tag>
+            ) : (
+              <Tag color={"volcano"}>No</Tag>
+            )}
+          </>
+        ),
+      },
+      {
+        title: "Wiki",
+        dataIndex: "wikipedia",
+        key: "wikipedia",
+        render: (text: string) => <a href={text}>Link</a>,
+      },
+    ];
+    subColumn = [
+      {
+        title: "Engine_loss_max",
+        dataIndex: "engine_loss_max",
+        key: "engine_loss_max",
+      },
+      {
+        title: "Engine_number",
+        dataIndex: "engine_number",
+        key: "engine_number",
+      },
+      {
+        title: "Engine_layout",
+        dataIndex: "engine_layout",
+        key: "engine_layout",
+      },
+    ];
     dataRocketDetail = [
       {
         key: data.rocket.id,
@@ -45,6 +93,11 @@ const Rocketdetail: any = (props: objIdRocket) => {
         height: data.rocket.height.meters,
         weight: data.rocket.mass.kg,
         company: data.rocket.company,
+        wikipedia: data.rocket.wikipedia,
+        tags: data.rocket.active,
+        engine_loss_max: data.rocket.engines.engine_loss_max,
+        engine_number: data.rocket.engines.number,
+        engine_layout: data.rocket.engines.layout,
       },
     ];
     console.log(data);
@@ -54,7 +107,21 @@ const Rocketdetail: any = (props: objIdRocket) => {
     <>
       {/* <label>{data.rocket ? data.rocket.description : ""}</label> */}
       {data.rocket ? (
-        <Table columns={column} dataSource={dataRocketDetail} />
+        <Table
+          columns={column}
+          expandable={{
+            expandedRowRender: (rec) => {
+              return (
+                <Table
+                  columns={subColumn}
+                  dataSource={dataRocketDetail}
+                  pagination={false}
+                />
+              );
+            },
+          }}
+          dataSource={dataRocketDetail}
+        />
       ) : (
         ""
       )}
