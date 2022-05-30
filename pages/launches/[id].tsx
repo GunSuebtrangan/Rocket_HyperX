971c2037bd6615717f7c8c757ede0fb9171923a0
GunSuebtrangan <gunsuebtrangan@gmail.com>
2022-05-30 13:57:28 +0700

    Update [id].tsx

--- pages/launches/[id].tsx
+++ pages/launches/[id].tsx
@@ -1,10 +1,24 @@
 import { gql, useQuery } from "@apollo/client";
 import { Table, Image, Spin } from "antd";
 import { useState } from "react";
-
+interface objLaunch {
+  id: string;
+  details: string;
+  mission_name: string;
+  links: { flickr_images: string };
+  launch_year: string;
+  launch_success: boolean;
+}
+interface Arr {
+  key: string;
+  images: Array<string> | string;
+  mission_name: string;
+  details: string;
+  launch_year: string;
+}
 const Launch = () => {
   const [visible, setVisible] = useState(false);
-  let arr: any = [];
+  let arr: Array<Arr> = [];
   const getLaunchData = gql`
     query GetLaunch {
       launches {
@@ -24,7 +38,7 @@ const Launch = () => {
   if (error) return `Error! ${error.message}`;
   if (data) {
     // console.log(data);
-    data.launches.map((value: any) => {
+    data.launches.map((value: objLaunch) => {
       arr.push({
         key: value.id,
         images: value.links.flickr_images,
@@ -52,6 +66,7 @@ const Launch = () => {
           render: (_, { images }) => (
             <>
               <Image
+                key={images[0]}
                 src={images[0]}
                 width={100}
                 preview={{
@@ -70,8 +85,13 @@ const Launch = () => {
                     onVisibleChange: (vis) => setVisible(vis),
                   }}
                 >
+<<<<<<< Updated upstream
                   {images.map((value: any) => {
                     return <Image src={value} />;
+=======
+                  {images.map((value: string) => {
+                    return <Image src={value} key={value} />;
+>>>>>>> Stashed changes
                   })}
                 </Image.PreviewGroup>
               </div>
